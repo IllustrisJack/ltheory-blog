495ed5af368b0628c1e82fdc300c0f7217ebfeb2
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2023-11-27 20:46:26 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+export default defineConfig({
+  plugins: [
+    inertia(),
+    vue({
+      // This is needed, or else Vite will try to find image paths (which it wont be able to find because this will be called on the web, not directly)
+      // For example <img src="/images/logo.png"> will not work without the code below
+      template: {
+        transformAssetUrls: {
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
+});
