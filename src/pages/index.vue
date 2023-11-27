9e924bdb5b3930e4e8c79ae14c8cb3ee947184f6
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2023-11-27 21:13:01 +0100

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col h-screen w-screen justify-center text-center">
     <img
-      src="/svg/LTR_TITLE.svg"
+      src=""
       class="mt-auto mr-auto mb-12 ml-auto px-12 w-auto drop-shadow-md noselect"
       draggable="false"
     />
